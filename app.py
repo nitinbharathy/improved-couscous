147e9f39ebade11f8e7f4ee5debbc7d5061eb2ca
nitinbharathy <nitin.bharathy@gmail.com>
2016-09-15 18:23:03 +0800

    Update app.py

--- app.py
+++ app.py
@@ -38,21 +38,23 @@ def makeWebhookResult(req):
         
     print("step 0")
     
-    session = req.get("sessionId")
+    sessionId = req.get("sessionId")
     result = req.get("result")
     parameters = result.get("parameters")
     
     
     print("step 1")
-    print(session)
-    test_blah = parameters.get("test_blah")
+    print(sessionId)
+    generic_category_query = parameters.get("generic_category_query")
     
     print("step 2")
 
     # cost = {'Europe':100, 'North America':200, 'South America':300, 'Asia':400, 'Africa':500}
+    
+    menutable={'vegetarian':'We just launched the veggie crunch','chicken':'The mc chicken and mc spicy are good choices','burger':'You should try the big mac or the mc spicy','beef':'Peoples favourites are the cheeseburger or the big mac','recommendations':'Please do not eat here - cook your own food!'}
 
     # speech = "The cost of shipping to " + zone + " is " + str(cost[zone]) + " euros."
-    speech = "You asked for " + test_blah + " my answer is blah for " + str(session) + " SESSION."
+    speech = "You asked for " + generic_category_query + ". "+ menutable[generic_category_query]+" for session "+ sessionId + "."
     
     print("Response:")
     print(speech)
